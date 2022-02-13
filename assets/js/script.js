084ac9ce2a3879990f3e5cc9f8d1d6a11116b385
AleksandrDenis <deniskudriavcev@gmail.com>
2022-02-13 16:09:58 +0000

    Delete console logs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,15 @@
 /* Event is fired when the whole page has loaded */ 
 window.addEventListener('load', function () {
-    //select all elements
+     //select all elements
     const playerScoreBoard = document.getElementById('player-score');
     const computerScoreBoard = document.getElementById('comp-score');
     const showComputerChoice = document.getElementById('comp-choice');
     const showPlayerChoice = document.getElementById('player-choice');
-    const playerChoices = document.querySelectorAll('.player-btn');
+    const availableChoices = document.querySelectorAll('.player-btn');
     console.log(playerChoices);
     const showOutcome = document.getElementsByClassName('result');
-    const sound = documet.querySelector('.sound');
+    const sound = document.querySelector('#sound');
+    console.log(sound);
 
 
 
